05628be78b8d5204815f52a0abda86bc37258ded
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-18 22:34:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,17 +18,11 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@artists
   end
 
-  #ll.count
-  # end
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
@@ -38,7 +32,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+  
 end
