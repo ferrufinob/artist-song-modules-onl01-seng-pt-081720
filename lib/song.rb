ad2a4938a2e025b26b879345db3bf23a48af667b
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-18 22:34:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,26 +11,11 @@ class Song
 
   @@songs = []
 
-  # def initialize
-  #   self.class.all << self
-  #   @@songs << self
-  # end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
+  
 
   def artist=(artist)
     @artist = artist
